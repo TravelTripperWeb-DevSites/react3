4e4ae8099334b6cb6b30d1f12112ba43a98d5a33
amekelburg <alexmek@gmail.com>
2020-07-17 13:53:52 -0400

    Update static.config.js

--- static.config.js
+++ static.config.js
@@ -127,7 +127,7 @@ export default {
   
   maxThreads: 1, // Remove this when you start doing any static generation
   devServer: {
-     host: process.env.CONTAINER_IP || "http://localhost:3000/",
+     host: process.env.CONTAINER_DOMAIN || "http://localhost:3000/",
      port: 3000
   },
   getSiteData: async (state) => {
