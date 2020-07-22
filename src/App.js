028c00972a1f8bc7bd49dad910d4efed51b0fef8
amekelburg <alexmek@gmail.com>
2020-07-22 08:23:48 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     //console.log(output)
     return (      
       <Root className="App">
-          <Suspense fallback="..loading.."> 
+          <Suspense fallback={()=>{console.log("FALLBACK"); return '...LOADING...';}> 
             <Router>
               <Routes default/>
             </Router>
